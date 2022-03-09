040a0cee892b1bd394dc0bb9e3cf86ffb049ae86
JordanMills233 <jordanmills233@gmail.com>
2022-03-09 20:06:43 +0000

    Cleaned up code adding image to gitignore

--- NasaAPI.py
+++ NasaAPI.py
@@ -1,31 +1,22 @@
 from datetime import datetime, timedelta
 from twitter import Twitter, OAuth
 from datetime import date, timedelta
-import requests
-import json 
-import shutil
-import config
+import requests, json, shutil, config
 
-today = date.today()
-yesterday = today - timedelta(days=365)
-print("Today's date:", today)
-print("1 Year ago date:", yesterday)
+yesterday = date.today() - timedelta(days=365)
 
 key = config.NasaKey
 
-r = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={}&api_key={}'.format(yesterday, key))
-a = r.url
+nasaRequest = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={}&api_key={}'.format(yesterday, key)
 
-response = json.loads(requests.get(a).text)
-
-t = response['photos'][000]['img_src']
+response = json.loads(requests.get(nasaRequest).text)
 
 ## Set up the image URL and filename
-image_url = t
+imageUrl = response['photos'][000]['img_src']
 filename = 'dailyimage.jpg'
 
 # Open the url image, set stream to True, this will return the stream content.
-r = requests.get(image_url, stream = True)
+r = requests.get(imageUrl, stream = True)
 
 # Check if the image was retrieved successfully
 if r.status_code == 200:
@@ -47,24 +38,23 @@ access_token = config.access_token
 access_token_secret = config.access_token_secret
 
 
-if __name__ == '__main__':
-    my_auth = OAuth(token=access_token,
-                    token_secret=access_token_secret,
-                    consumer_key=consumer_key,
-                    consumer_secret=consumer_secret_key)
+my_auth = OAuth(token=access_token,
+                token_secret=access_token_secret,
+                consumer_key=consumer_key,
+                consumer_secret=consumer_secret_key)
 
-    t = Twitter(auth=my_auth)
+t = Twitter(auth=my_auth)
 
-    # Send images along with your tweets:
-    # - first just read images from the web or from files the regular way:
-    with open("dailyimage.jpg", "rb") as imagefile:
-        imagedata = imagefile.read()
-    # - then upload medias one by one on Twitter's dedicated server
-    #   and collect each one's id:
-    t_up = Twitter(domain='upload.twitter.com', auth=my_auth)
-    id_img = t_up.media.upload(media=imagedata)["media_id_string"]
-    t.statuses.update(status="Curiosity rover {}".format(yesterday),
-                      media_ids=",".join([id_img]))
+# Send images along with your tweets:
+# - first just read images from the web or from files the regular way:
+with open("dailyimage.jpg", "rb") as imagefile:
+    imagedata = imagefile.read()
+# - then upload medias one by one on Twitter's dedicated server
+#   and collect each one's id:
+t_up = Twitter(domain='upload.twitter.com', auth=my_auth)
+id_img = t_up.media.upload(media=imagedata)["media_id_string"]
+t.statuses.update(status="Curiosity rover {}".format(yesterday),
+                    media_ids=",".join([id_img]))
 
 
 
